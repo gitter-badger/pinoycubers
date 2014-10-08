ac77ac8ee8317d74d96bc8c367942148a8149b8d
Aivan Monceller <aivan@monceller.com>
2014-10-09 00:08:47 +0800

    Update database seed for user, add username

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -22,12 +22,17 @@ class DatabaseSeeder extends Seeder {
         $this->command->info('User roles table seeded');
 
         // Using the eloquent model User to create a user row
-        User::create(array(
+        $user = User::create(array(
             'email' => 'aivan@pinoycubers.org',
             'password' => Hash::make('password'),
             'first_name'  => 'Aivan',
             'last_name' => 'Monceller',
             'role_id' => UserRole::$ADMIN));
+
+        Profile::create(array(
+            'user_id' => $user->id,
+            'username' => "geocine"
+        ));
 	}
 
 }
