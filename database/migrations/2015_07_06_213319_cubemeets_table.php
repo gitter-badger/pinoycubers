9449c7dc1d42c64268a269c4566a9ce41dc0c7c2
Dan Belza <dhanrheb@gmail.com>
2015-07-07 19:07:52 +0800

    Create migration for cubemeets table

---
+++ database/migrations/2015_07_06_213319_cubemeets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CubemeetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cubemeets', function($table)
+        {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('host');
+            $table->string('location');
+            $table->string('description');
+            $table->date('date');
+            $table->time('start_time');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cubemeets');
+    }
+}
