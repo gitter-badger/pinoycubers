92c302fd9d666101ac5c3fd5d2de74f53c319220
Dan Belza <dhanrheb@gmail.com>
2015-07-08 06:35:27 +0800

    Updated view

--- resources/views/cubemeet/index.blade.php
+++ resources/views/cubemeet/index.blade.php
@@ -8,7 +8,23 @@
 @section('content')
 
 <div class="row">
-    Cube Meets
+    <h2>Cube Meets</h2>
+    {!! Html::link('/cubemeets/create', 'Set Cube Meet', ['class' => 'btn btn-sm btn-default']) !!}
+
+    <hr>
+
+    <h3>CM Name <small>by name</small></h3>
+    <ul>
+        <li>Location:</li>
+        <li>Date:</li>
+        <li>Start Time:</li>
+    </ul>
 </div>
+<div class="row">
+    {!! Html::link('#', 'Join', ['class' => 'btn btn-sm btn-primary']) !!}
+    {!! Html::link('#', 'View Details', ['class' => 'btn btn-sm btn-primary']) !!}
+</div>
+
+<hr>
 
 @stop
