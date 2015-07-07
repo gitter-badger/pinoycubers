9ce890f4362d0574c0e8907ad63e028e77136e76
Dan Belza <dhanrheb@gmail.com>
2015-07-08 07:31:46 +0800

    Add fillable fields

--- app/Cubemeet.php
+++ app/Cubemeet.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cubemeet extends Model
 {
-    //
+    /**
+     * Fillable fields
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'name',
+        'location',
+        'description',
+        'date',
+        'start_time',
+    ];
 }
