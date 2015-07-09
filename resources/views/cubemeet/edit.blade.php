ff2c10d332127b20f7196a18a2f2adc9b205c041
Dan Belza <dhanrheb@gmail.com>
2015-07-10 07:56:49 +0800

    Add edit view file

---
+++ resources/views/cubemeet/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+
+@section('title')
+@parent
+| Edit Cube Meets
+@stop
+
+@section('content')
+
+<div class="row">
+    <div class="well">
+        <h4>Edit Cube Meet</h4>
+
+        <hr>
+
+        {!! Form::model($cubemeet, ['url' => 'cubemeets', 'route' => 'task.update', 'method' => 'patch', 'role' => 'form']) !!}
+
+            <div class="form-group">
+                {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'CM Name']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::text('location', null, ['class' => 'form-control', 'placeholder' => 'Location']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::textarea('desecription', null, ['class' => 'form-control', 'placeholder' => 'Description']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::input('date', 'date', Carbon\Carbon::now()->toDateString(), ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::input('time', 'time', Carbon\Carbon::now()->toTimeString(), ['class' => 'form-control']) !!}
+            </div>
+
+            {!! Form::submit('Save', ['class' => 'btn btn-md btn-primary', 'id' => 'savebutton']) !!}
+
+        {!! Form::close() !!}
+    </div>
+</div>
+
+@stop
