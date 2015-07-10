a77d5e005707976b55d0f76c6b1d87b39b2d34bd
Dan Belza <dhanrheb@gmail.com>
2015-07-11 03:56:44 +0800

    Update layout (split date fields)

--- resources/views/cubemeet/create.blade.php
+++ resources/views/cubemeet/create.blade.php
@@ -24,14 +24,22 @@
             <div class="form-group">
                 {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'Description']) !!}
             </div>
-            <div class="form-group">
-                {!! Form::input('date', 'date', Carbon\Carbon::now()->toDateString(), ['class' => 'form-control']) !!}
+            <div class="row form-group">
+                <div class="col-md-2">
+                    {!! Form::selectMonth('month', Carbon\Carbon::now()->month, ['class' => 'form-control']) !!}
+                </div>
+                <div class="col-md-2">
+                    {!! Form::input('number', 'day', Carbon\Carbon::now()->day, ['class' => 'form-control', 'placeholder' => 'Day', 'maxlength' => '2', 'min' => '1', 'max' => '31']) !!}
+                </div>
+                <div class="col-md-2">
+                    {!! Form::input('number', 'year', Carbon\Carbon::now()->year, ['class' => 'form-control', 'placeholder' => 'Year', 'maxlength' => '4', 'min' => '1980', 'max' => '2050']) !!}
+                </div>
             </div>
             <div class="form-group">
-                {!! Form::input('time', 'time', Carbon\Carbon::now()->toTimeString(), ['class' => 'form-control']) !!}
+                {!! Form::input('time', 'time', '12:00', ['class' => 'form-control']) !!}
             </div>
 
-            {!! Form::submit('Register', ['class' => 'btn btn-md btn-primary', 'id' => 'registerbutton']) !!}
+            {!! Form::submit('Set Cube Meet', ['class' => 'btn btn-md btn-primary', 'id' => 'registerbutton']) !!}
 
         {!! Form::close() !!}
     </div>
