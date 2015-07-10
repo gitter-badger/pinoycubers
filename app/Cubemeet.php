eaf9d480339940a969279cafd207dd59e6767e76
Dan Belza <dhanrheb@gmail.com>
2015-07-11 04:08:12 +0800

    Add date field to date mutator

--- app/Cubemeet.php
+++ app/Cubemeet.php
@@ -20,6 +20,14 @@ class Cubemeet extends Model
         'start_time',
     ];
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+
+    protected $dates = ['date'];
+
     public function host()
     {
         return $this->belongsTo('App\User', 'host');
