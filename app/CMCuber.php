c763f060fdac26e5d7783349fecca7982ff42410
Dan Belza <dhanrheb@gmail.com>
2015-07-11 00:30:39 +0800

    Add fillable fields

--- app/CMCuber.php
+++ app/CMCuber.php
@@ -7,10 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class CMCuber extends Model
 {
     /**
+     * Fillable fields
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'user_id',
+        'status',
+    ];
+
+    /**
      * The database table used by the model.
      *
      * @var string
      */
+    
     protected $table = 'cubemeet_cubers';
 
     public function cubemeet() {
