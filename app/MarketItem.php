430dcef4db80cdf7b49d8d8bf8cd339d563e8981
Dan Belza <dhanrheb@gmail.com>
2015-12-29 15:28:07 +0800

    Add fillable fields

--- app/MarketItem.php
+++ app/MarketItem.php
@@ -7,6 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 class MarketItem extends Model
 {
     /**
+     * Fillable fields
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'title',
+        'description',
+        'contact',
+        'type',
+        'other_type',
+        'manufacturer',
+        'other_manufacturer',
+        'condition',
+        'condition_details',
+        'container',
+        'shipping',
+        'shipping_details',
+        'meetups',
+        'meetup_details',
+        'slug'
+    ];
+
+    /**
       * Get the users that owns the item.
       */
     public function user()
