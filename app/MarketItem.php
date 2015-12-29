760a4c64c2855714daafa00f609f4156085de55e
Dan Belza <dhanrheb@gmail.com>
2015-12-29 12:40:00 +0800

    Establish user-marketitem relationship

--- app/MarketItem.php
+++ app/MarketItem.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class MarketItem extends Model
 {
-    //
+    /**
+      * Get the users that owns the item.
+      */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
 }
