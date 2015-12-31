983e18db266b0a79b4e4819f158d16a69fa40dfd
Dan Belza <dhanrheb@gmail.com>
2015-12-31 08:35:40 +0800

    Decrease market item pagination from 20 to 10

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -19,7 +19,7 @@ class MarketController extends Controller
      */
     public function showIndex()
     {
-        $items = MarketItem::with('user')->paginate(20);
+        $items = MarketItem::with('user')->paginate(10);
 
         return View::make('market.index', compact('items'));
     }
