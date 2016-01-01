102b53d00a2008dbdcb01de30f86fe20314875f2
Dan Belza <dhanrheb@gmail.com>
2016-01-01 11:07:55 +0800

    Joins 'comments' to the items query

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -20,7 +20,7 @@ class MarketController extends Controller
      */
     public function showIndex()
     {
-        $items = MarketItem::with('user')->orderBy('created_at', 'desc')->paginate(10);
+        $items = MarketItem::with('user', 'comments')->orderBy('created_at', 'desc')->paginate(10);
 
         return View::make('market.index', compact('items'));
     }
