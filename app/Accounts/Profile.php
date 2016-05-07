45a20377772364428e8b71d116fe1f6322894181
Dan Belza <dhanrheb@gmail.com>
2016-05-07 15:28:00 +0800

    Add fillables

--- app/Accounts/Profile.php
+++ app/Accounts/Profile.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    /**
+     * Fillable fields
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username'
+    ];
+
     public function user()
     {
         return $this->belongsTo('App\User');
