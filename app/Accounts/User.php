4c8432661333cfa2a65730eadc5880c8ccd3e100
Dan Belza <dhanrheb@gmail.com>
2016-05-07 14:58:07 +0800

    Fix class path

--- app/Accounts/User.php
+++ app/Accounts/User.php
@@ -73,7 +73,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 
     public function profile()
     {
-        return $this->hasOne('App\Profile');
+        return $this->hasOne('App\Accounts\Profile');
     }
 
     /**
