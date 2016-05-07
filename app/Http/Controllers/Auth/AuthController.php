975436597cbf04773707764e9c26a89442c4c730
Dan Belza <dhanrheb@gmail.com>
2016-05-07 15:18:25 +0800

    Fix logout middleware exception

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'getLogout']);
+        $this->middleware('guest', ['except' => 'logout']);
     }
 
     public function login()
