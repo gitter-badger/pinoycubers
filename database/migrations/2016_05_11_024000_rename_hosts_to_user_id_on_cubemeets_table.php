fd0424bd4ae39d3c0e65a68d6ab410204de5dbdf
Dan Belza <dhanrheb@gmail.com>
2016-05-11 02:41:52 +0800

    Add migration file to rename hosts to user_id on cubemeets table

---
+++ database/migrations/2016_05_11_024000_rename_hosts_to_user_id_on_cubemeets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameHostsToUserIdOnCubemeetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cubemeets', function (Blueprint $table) {
+            $table->renameColumn('host', 'user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cubemeet_cubers', function (Blueprint $table) {
+            $table->renameColumn('user_id', 'host');
+        });
+    }
+}
