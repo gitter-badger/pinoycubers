b8f5da656eab16ad118d85e04543232d4033664f
Dan Belza <dhanrheb@gmail.com>
2016-05-11 05:25:12 +0800

    Implement AttendeeCreator

--- app/Http/Controllers/Cubemeets/CubemeetAttendeesController.php
+++ app/Http/Controllers/Cubemeets/CubemeetAttendeesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Cubemeets;
 
 use Auth;
 use Redirect;
@@ -9,8 +9,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Cubemeets\CubemeetRepository;
 use App\Cubemeets\Attendees\Attendee;
+use App\Cubemeets\Attendees\AttendeeCreator;
+use App\Cubemeets\Attendees\AttendeeCreatorListener;
 
-class CubemeetAttendeesController extends Controller
+class CubemeetAttendeesController extends Controller implements AttendeeCreatorListener
 {
     /**
      * @var \App\Cubemeets\CubemeetRepository
@@ -18,26 +20,30 @@ class CubemeetAttendeesController extends Controller
     protected $cubemeets;
 
     /**
+     * @var \App\Cubemeets\Attendees\AttendeeCreator
+     */
+    protected $attendeeCreator;
+
+    /**
      * @param \App\Cubemeets\CubemeetRepository $cubemeets
      * @return void
      */
-    public function __construct(CubemeetRepository $cubemeets)
+    public function __construct(CubemeetRepository $cubemeets, AttendeeCreator $attendeeCreator)
     {
         $this->cubemeets = $cubemeets;
+        $this->attendeeCreator = $attendeeCreator;
     }
 
-    public function join($id)
+    public function join($id, Request $request)
     {
         $cubemeet = $this->cubemeets->getById($id);
 
-        $cuber = (new Attendee([
-            'user_id' => Auth::user()->id,
-            'status' => 'Going',
-        ]))->toArray();
+        $data = [
+            'cm_id' => $cubemeet->id,
+            'user_id' => $request->user()->id
+        ];
 
-        $cubemeet->cubers()->updateOrCreate(['user_id' => Auth::user()->id], $cuber);
-
-        return Redirect::back()->with('success', 'Successfuly joined');
+        return $this->attendeeCreator->create($this, $data);
     }
 
     public function canceljoin($id)
@@ -47,5 +53,10 @@ class CubemeetAttendeesController extends Controller
         $cubemeet->cubers()->where('user_id', Auth::user()->id)->update(['status' => 'Not Going']);
 
         return Redirect::back()->with('success', 'Success');
+    }
+
+    public function attendeeCreated()
+    {
+        return Redirect::back()->with('success', 'Successfuly joined');
     }
 }
