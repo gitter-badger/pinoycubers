a6b0a7fccd992b1aa781696ea222471722556097
Dan Belza <dhanrheb@gmail.com>
2016-05-11 07:29:36 +0800

    Add UpdateProfileRequest

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UpdateProfileRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email|unique:users,email,' . $this->user()->id
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'firstname.required' => 'The first name field is required.',
+            'lastname.required' => 'The last name field is required.',
+            'email.unique' => 'This email is already used'
+        ];
+    }
+}
