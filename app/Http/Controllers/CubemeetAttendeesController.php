0db98ef1ad0086f2c18ec6c8cdb57cda9320aac7
Dan Belza <dhanrheb@gmail.com>
2016-05-11 03:51:57 +0800

    Fix missing dependencies

--- app/Http/Controllers/CubemeetAttendeesController.php
+++ app/Http/Controllers/CubemeetAttendeesController.php
@@ -7,10 +7,25 @@ use Redirect;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Cubemeets\CubemeetRepository;
 use App\Cubemeets\Attendees\Attendee;
 
 class CubemeetAttendeesController extends Controller
 {
+    /**
+     * @var \App\Cubemeets\CubemeetRepository
+     */
+    protected $cubemeets;
+
+    /**
+     * @param \App\Cubemeets\CubemeetRepository $cubemeets
+     * @return void
+     */
+    public function __construct(CubemeetRepository $cubemeets)
+    {
+        $this->cubemeets = $cubemeets;
+    }
+
     public function join($id)
     {
         $cubemeet = $this->cubemeets->getById($id);
