080f9afd3ea9fa9b1af67496f26d801e0373ba91
Dan Belza <dhanrheb@gmail.com>
2016-05-11 03:14:55 +0800

    Refix typo

--- database/migrations/2016_05_11_024000_rename_hosts_to_user_id_on_cubemeets_table.php
+++ database/migrations/2016_05_11_024000_rename_hosts_to_user_id_on_cubemeets_table.php
@@ -12,7 +12,7 @@ class RenameHostsToUserIdOnCubemeetsTable extends Migration
      */
     public function up()
     {
-        Schema::table('cubemeets_cubers', function (Blueprint $table) {
+        Schema::table('cubemeets', function (Blueprint $table) {
             $table->renameColumn('host', 'user_id');
         });
     }
@@ -24,7 +24,7 @@ class RenameHostsToUserIdOnCubemeetsTable extends Migration
      */
     public function down()
     {
-        Schema::table('cubemeet_cubers', function (Blueprint $table) {
+        Schema::table('cubemeet', function (Blueprint $table) {
             $table->renameColumn('user_id', 'host');
         });
     }
