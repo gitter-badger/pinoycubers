681fb7818c8e50362eea266b16b7bba25d5061df
Dan Belza <dhanrheb@gmail.com>
2016-05-17 03:37:20 +0800

    Refactor cubemeets index

--- resources/views/cubemeets/index.blade.php
+++ resources/views/cubemeets/index.blade.php
@@ -13,7 +13,9 @@
         <h3>Cube Meets</h3>
     </div>
     <div class="col-md-2 text-right" style="margin-top: 18px; padding: 0">
-        <a href="/cubemeets/set/" class="btn btn-sm btn-default">Set Cube Meet</a>
+        <a href="/cubemeets/set/" class="btn btn-sm btn-default">
+            <span class="fa fa-fw fa-plus"></span> Set a Cube Meet
+        </a>
     </div>
 </div>
 
@@ -27,47 +29,49 @@
 
 @foreach ($cubemeets as $cm)
 <div class="row">
-    <div class="col-sm-6">
+    <div class="col-sm-5">
         <ul class="list-unstyled">
             <li>
-                <b><a href="{{ '/cubemeets/'.$cm->id }}">{{ $cm->name }}</a></b> 
+                <b><a href="{{ '/cubemeets/'.$cm->id }}">{{ $cm->name }}</a></b>
                 <small>by {{ $cm->hostName() }}</small>
             </li>
-            <li><span class="fa fa-map-marker"></span> {{ $cm->location }}</li>
-            <li><span class="fa fa-calendar"></span> {{ $cm->date->format('M d, Y') }}</li>
-            <li><span class="fa fa-clock-o"></span> {{ date('h:i A', strtotime($cm->start_time)) }}</li>
+            <li><span class="fa fa-fw fa-calendar"></span> {{ $cm->date->format('M d, Y') }} · {{ date('h:i A', strtotime($cm->start_time)) }}</li>
         </ul>
     </div>
     <div class="col-sm-5">
         <ul class="list-unstyled">
-            <li><span class="fa fa-comment"></span> {{ str_limit($cm->description, 100) }}</li>
-            <li><span class="fa fa-user"></span> {{ $cm->cubers()->where('status', 'Going')->count() + 1 }} expected Cuber(s) to attend</li>
+            <li><span class="fa fa-fw fa-map-marker"></span> {{ $cm->location }}</li>
+            <li>
+                <span class="fa fa-fw fa-user"></span> 
+                {{ $cuberCount = $cm->countCubers() }} 
+                {{ $cuberCount > 1? 'Cubers': 'Cuber' }}
+            </li>
         </ul>
     </div>
-    <div class="col-sm-1">
-        @if ($cm->host()->getResults()->id == Auth::user()->id)
-            {!! Form::open(['method' => 'POST', 'url' => '/cubemeets/'.$cm->id.'/cancel', 'class' => 'text-right']) !!}
-                <a href="{{ '/cubemeets/'.$cm->id.'/edit' }}" class="btn btn-sm btn-default">Edit</a>
-                {!! Form::submit('Cancel', ['class' => 'btn btn-sm btn-danger']) !!}
+    <div class="col-sm-2">
+        @if ($cm->signedUserIsHost())
+            {!! Form::open(['url' => '/cubemeets/'.$cm->id.'/cancel', 'class' => 'text-right']) !!}
+                <a href="{{ '/cubemeets/'.$cm->id.'/edit' }}" class="btn btn-sm btn-default">
+                    <span class="fa fa-fw fa-pencil"></span> Edit
+                </a>
+                <button type="submit" class="btn btn-sm btn-danger">
+                    <span class="fa fa-fw fa-times"></span> Cancel
+                </button>
             {!! Form::close() !!}
         @else
             <div class="text-right">
-                @if (array_first($cm->cubers, function ($key, $value) {
-                    if($value['user_id'] == Auth::user()->id) {
-                        if($value['status'] == 'Going') {
-                            return true;
-                        }
-                        return false;
-                    }
-                    return false;
-                }))
-                {!! Form::open(['url' => 'cubemeets/'.$cm->id.'/canceljoin', 'role' => 'form']) !!}
-                    {!! Form::submit('Not Going', ['class' => 'btn btn-sm btn-primary']) !!}
-                {!! Form::close() !!}
+                @if ($cm->attendeeIsGoing())
+                    {!! Form::open(['url' => 'cubemeets/'.$cm->id.'/canceljoin', 'role' => 'form']) !!}
+                        <button type="submit" class="btn btn-sm btn-primary">
+                            <span class="fa fa-ban"> Not Going</span>
+                        </button>
+                    {!! Form::close() !!}
                 @else
-                {!! Form::open(['url' => 'cubemeets/'.$cm->id.'/join', 'role' => 'form']) !!}
-                    {!! Form::submit('Join', ['class' => 'btn btn-sm btn-primary']) !!}
-                {!! Form::close() !!}
+                    {!! Form::open(['url' => 'cubemeets/'.$cm->id.'/join', 'role' => 'form']) !!}
+                        <button type="submit" class="btn btn-sm btn-primary">
+                            <span class="fa fa-check"> Join</span>
+                        </button>
+                    {!! Form::close() !!}
                 @endif
             </div>
         @endif
