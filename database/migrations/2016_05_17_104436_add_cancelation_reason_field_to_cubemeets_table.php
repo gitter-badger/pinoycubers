85dd84213942255814da740b1143ddf054d51059
Dan Belza <dhanrheb@gmail.com>
2016-05-17 10:48:59 +0800

    Add migration file to add cancelation_reason field to cubemeets table

---
+++ database/migrations/2016_05_17_104436_add_cancelation_reason_field_to_cubemeets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCancelationReasonFieldToCubemeetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cubemeets', function(Blueprint $table) {
+            $table->string('cancelation_reason');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cubemeets', function(Blueprint $table) {
+            $table->string('cancelation_reason');
+        });
+    }
+}
