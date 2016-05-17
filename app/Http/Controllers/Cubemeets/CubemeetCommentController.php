454a5f1b5a71eb4e3f75eb3fd0cf873580336dd4
Dan Belza <dhanrheb@gmail.com>
2016-05-17 22:47:07 +0800

    Add CubemeetCommentController.php

---
+++ app/Http/Controllers/Cubemeets/CubemeetCommentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Cubemeets;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CubemeetCommentController extends Controller
+{
+    //
+}
