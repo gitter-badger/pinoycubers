6263e0c4e2d18d460f2023408d536aee66816c00
Dan Belza <dhanrheb@gmail.com>
2016-05-17 22:24:52 +0800

    Refactor cubemeet views

--- resources/views/cubemeets/index.blade.php
+++ resources/views/cubemeets/index.blade.php
@@ -49,34 +49,7 @@
         </ul>
     </div>
     <div class="col-sm-3 text-right">
-        @if ($cm->status == 'Scheduled')
-            @if ($cm->signedUserIsHost())
-                <a href="{{ '/cubemeets/'.$cm->slug.'/edit' }}" class="btn btn-sm btn-default">
-                    <span class="fa fa-fw fa-pencil"></span> Edit
-                </a>
-                <a href="{{ '/cubemeets/'.$cm->slug.'/cancel' }}" class="btn btn-sm btn-danger">
-                    <span class="fa fa-fw fa-times"></span> Cancel
-                </a>
-            @else
-                @if ($cm->attendeeIsGoing())
-                    {!! Form::open(['url' => 'cubemeets/'.$cm->slug.'/canceljoin', 'role' => 'form']) !!}
-                        <button type="submit" class="btn btn-sm btn-primary">
-                            <span class="fa fa-ban"> Not Going</span>
-                        </button>
-                    {!! Form::close() !!}
-                @else
-                    {!! Form::open(['url' => 'cubemeets/'.$cm->slug.'/join', 'role' => 'form']) !!}
-                        <button type="submit" class="btn btn-sm btn-primary">
-                            <span class="fa fa-check"> Join</span>
-                        </button>
-                    {!! Form::close() !!}
-                @endif
-            @endif
-        @elseif ($cm->status == 'Canceled')
-            <ul class="list-unstyled">
-                <li><span class="fa fa-fw fa-ban"></span> Cube meet is canceled.</li>
-            </ul>
-        @endif
+        @include('cubemeets._actions')
     </div>
 </div>
 <hr>
