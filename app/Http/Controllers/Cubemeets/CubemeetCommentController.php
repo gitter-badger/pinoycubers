e3e95df12934d66c69db47e2bc445208d9c2e087
Dan Belza <dhanrheb@gmail.com>
2016-05-18 00:12:59 +0800

    Implement cubemeet comment editing

--- app/Http/Controllers/Cubemeets/CubemeetCommentController.php
+++ app/Http/Controllers/Cubemeets/CubemeetCommentController.php
@@ -6,12 +6,15 @@ use App\Cubemeets\CubemeetRepository;
 use App\Cubemeets\Comments\CommentCreator;
 use App\Cubemeets\Comments\CommentCreatorListener;
 use App\Cubemeets\Comments\CommentRepository;
+use App\Cubemeets\Comments\CommentUpdater;
+use App\Cubemeets\Comments\CommentUpdaterListener;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Redirect;
+use View;
 
-class CubemeetCommentController extends Controller implements CommentCreatorListener
+class CubemeetCommentController extends Controller implements CommentCreatorListener, CommentUpdaterListener
 {
     /**
      * @var \App\Cubemeets\CubemeetRepository
@@ -29,15 +32,22 @@ class CubemeetCommentController extends Controller implements CommentCreatorList
     protected $commentCreator;
 
     /**
+     * @var \App\Cubemeets\Comments\CommentUpdater
+     */
+    protected $commentUpdater;
+
+    /**
      * @param \App\Cubemeets\CubemeetRepository $cubemeets
      * @param \App\Cubemeets\Comments\CommentRepository $comments
      * @param \App\Cubemeets\Comments\CommentCreator $commentCreator
+     * @param \App\Cubemeets\Comments\CommentUpdater $commentUpdater
      */
-    public function __construct(CubemeetRepository $cubemeets, CommentRepository $comments, CommentCreator $commentCreator)
+    public function __construct(CubemeetRepository $cubemeets, CommentRepository $comments, CommentCreator $commentCreator, CommentUpdater $commentUpdater)
     {
         $this->cubemeets = $cubemeets;
         $this->comments = $comments;
         $this->commentCreator = $commentCreator;
+        $this->commentUpdater = $commentUpdater;
     }
 
     public function comment($slug, Request $request)
@@ -53,8 +63,31 @@ class CubemeetCommentController extends Controller implements CommentCreatorList
         return $this->commentCreator->create($this, $data);
     }
 
+    public function edit($id)
+    {
+        $comment = $this->comments->getById($id);
+
+        return View::make('cubemeets.comments.edit', compact('comment'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $comment = $this->comments->getById($id);
+
+        $data = ['comment' => $request->comment];
+
+        return $this->commentUpdater->update($this, $comment, $data);
+    }
+
     public function commentCreated()
     {
         return Redirect::back();
+    }
+
+    public function commentUpdated($comment)
+    {
+        $cubemeet = $comment->getCubemeet();
+
+        return Redirect::to('cubemeets/'.$cubemeet->slug)->with('success', 'Comment updated');
     }
 }
