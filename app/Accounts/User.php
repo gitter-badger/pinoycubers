7df477eb75fd49907fc4194d7e9eeaab3a54712a
Dan Belza <dhanrheb@gmail.com>
2016-05-20 06:49:37 +0800

    Add 'photo' field to mass assignables

--- app/Accounts/User.php
+++ app/Accounts/User.php
@@ -29,7 +29,8 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 
     protected $fillable = [
         'email',
-        'password'
+        'password',
+        'photo'
     ];
 
 	/**
